093b448b193e6402e950280b50a75ccac20590b3
deysudip <dey.sudipkr@gmail.com>
2019-05-03 03:00:27 +0530

    scan test 03

---
+++ src/app/classes/cart.ts
@@ -0,0 +1,7 @@
+export class Cart {
+  productId: string;
+  name: string;
+  qty: number;
+  price: number;
+  items: string[];
+}
---
+++ src/app/dummyData/Products.ts
@@ -0,0 +1,32 @@
+export const PRODUCTS_DUMMY = [
+  {
+    id: '978020137962',
+    productId: '000ABC',
+    name: 'Product A',
+    price: 100
+  },
+  {
+    id: '978020137972',
+      productId: '000ABC',
+    name: 'Product A',
+    price: 100
+  },
+  {
+    id: '978020137862',
+    productId: '000XYZ',
+    name: 'Product B',
+    price: 200
+  },
+  {
+    id: '978020137872',
+    productId: '000XYZ',
+    name: 'Product B',
+    price: 200
+  },
+  {
+    id: '978020137762',
+    productId: '000MNO',
+    name: 'Product A',
+    price: 300
+  }
+];
