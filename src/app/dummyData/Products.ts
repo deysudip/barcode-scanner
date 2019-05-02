73f8ca604df578f51245ac586533ef7a7c514ddf
deysudip <dey.sudipkr@gmail.com>
2019-05-03 03:53:34 +0530

    scan test 04

--- src/app/dummyData/Products.ts
+++ src/app/dummyData/Products.ts
@@ -1,30 +1,30 @@
 export const PRODUCTS_DUMMY = [
   {
-    id: '978020137962',
+    id: '78020137962',
     productId: '000ABC',
     name: 'Product A',
     price: 100
   },
   {
-    id: '978020137972',
+    id: '78020137972',
       productId: '000ABC',
     name: 'Product A',
     price: 100
   },
   {
-    id: '978020137862',
+    id: '78020137862',
     productId: '000XYZ',
     name: 'Product B',
     price: 200
   },
   {
-    id: '978020137872',
+    id: '78020137872',
     productId: '000XYZ',
     name: 'Product B',
     price: 200
   },
   {
-    id: '978020137762',
+    id: '78020137762',
     productId: '000MNO',
     name: 'Product A',
     price: 300
